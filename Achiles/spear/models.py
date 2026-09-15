bd1b7ac3e1112999862ff442de9cdbe119d8ddac
Студент группы IS <is@sielom.local>
2026-09-15 16:35:09 +0500

    second

--- Achiles/spear/models.py
+++ Achiles/spear/models.py
@@ -7,3 +7,24 @@ class Habits(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     description = models.TextField()
+    repeat_time = models.IntegerField
+    times_per_day = models.IntegerField
+    max_streaks_days = models.IntegerField
+    is_active = models.BooleanField
+
+
+class habit_completions(models.model):
+    habit_id = models.ForeignKey(Habits, on_delete=models.CASCADE)
+    completed_at = models.DateTimeField
+
+
+class tags(models.model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+
+class habit_tags(models.model):
+    habit_id = models.ForeignKey(Habits, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(tags, on_delete=models.CASCADE)
+
+
+
--- Achiles/spear/views.py
+++ Achiles/spear/views.py
@@ -1,3 +1,8 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
-
+from models import *
 # Create your views here.
+
+def get_users(request):
+    users = User.objects.all()

4ae92e86b1683394c09e1612062162ad24767285
Студент группы IS <is@sielom.local>
2026-09-15 16:38:54 +0500

    gg

--- Achiles/spear/views.py
+++ Achiles/spear/views.py
@@ -2,7 +2,11 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from models import *
+from django.views import View
 # Create your views here.
+
+
+
 
 def get_users(request):
     users = User.objects.all()
